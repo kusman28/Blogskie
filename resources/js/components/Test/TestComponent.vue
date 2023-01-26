806dbdf19c5824ae329c2ac265a33510452d612a
Khalid Usman <kusman758@gmail.com>
2023-01-27 00:13:07 +0800

    Initial XD

---
+++ resources/js/components/Test/TestComponent.vue
@@ -0,0 +1,14 @@
+<template>
+	<div>
+		<p>
+			Test 123
+		</p>
+	</div>
+</template>
+<script>
+	export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
